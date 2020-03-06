9ce3d37a5579396fc193ae78454f10cc31f74e80
Nádia Oliveira <nadiaaoliverr@gmail.com>
2020-03-06 17:47:49 -0300

    Adicionando cadastro de autores

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print("Setup is ok")
+from datetime import datetime
+
+def registration_author():
+    author = []
+    print('\n--- Registration new author ---')
+    name = input('Name: ')
+    email = input('E-mail: ')
+    description = input('Description: ')
+    time_registration = datetime.now().strftime("%d/%m/%Y às %H:%M:%S de BRT")
+    author.append(email)
+    author.append(description)
+    author.append(time_registration)
+    return name, author
+
+if __name__ == '__main__':
+
+    authors = {}
+    quantity = int(input('Number of authors: '))
+    for i in range(quantity):
+        name, authors[name] = registration_author()
+
+    print(authors)
