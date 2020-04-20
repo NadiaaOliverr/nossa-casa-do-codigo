11872d825eb1edf3d1f78c1c0808fdf2dc40ac3d
Nádia Oliveira <nadiaaoliverr@gmail.com>
2020-04-20 20:08:09 -0300

    Tirando __iter__

--- dao/book_dao.py
+++ dao/book_dao.py
@@ -8,15 +8,6 @@ class BookDatabase:
     def __init__(self) -> None:
         self.__list = []
 
-    '''
-        O método __iter__ é para que possa iterar diretamente sobre o objeto criado.
-        Evitando assim acessar por exemplo: for book in books.list
-        Faça então: for book in books
-    '''
-
-    def __iter__(self):
-        return self.list.__iter__()
-
     def add(self, book: Book) -> None:
         if book in self.__list:
             raise Exception('Título ou ISBN duplicado. Isto não é permitido')
